43d30bb6c04c277c0d613523481b006f282999ff
Tanmay Chavan <tanmaychavan2001@gmail.com>
2021-03-10 19:00:27 +0530

    actual 1st commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+void fn() {
+  print('hello world!');
+//  return 0;
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Color(0xe60073ff),
+          body: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              textBaseline: TextBaseline.ideographic,
+              children: <Widget>[
+                // 'images/circle-cropped.png'
+                SizedBox(
+                  height:100,
+                ),
+                Image(
+                  image: AssetImage('images/clipart499896.png'),
+                  height: 75,
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 70, 30, 30),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "email ID",
+                        fillColor: Colors.white,
+                        filled: true),
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 0, 30, 40),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "password",
+                        fillColor: Colors.white,
+                        filled: true),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  height: 50,
+                  child: TextButton(
+                    onPressed: fn,
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.amber),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.pinkAccent)),
+                    child: Text("LOG IN"),
+                  ),
+                ),
+
+                Container(
+                  height: 10,
+                  margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                  child: Divider(thickness: 2, color: Colors.yellow,),
+                ),
+
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 15, 30, 20),
+                  height: 50,
+                  child: TextButton(
+                    onPressed: fn,
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.pinkAccent),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.amber)),
+                    child: Text("SIGN UP"),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
